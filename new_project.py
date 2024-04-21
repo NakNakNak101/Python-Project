00bc53a83957d966e6886b1a1b073e60287fe65c
unknown <waddhanakrin@gmail.com>
2024-04-21 17:37:08 +0700

    learning matplotlib

---
+++ new_project.py
@@ -0,0 +1,18 @@
+from matplotlib import pyplot as plt
+
+
+#x_value =  [1, 2, 3, 4]
+#y_value = [5,4,3,2]
+
+#plt.plot(x_value, y_value)
+#plt.scatter(x_value, y_value)
+
+other_x_value = [2,4,6,7]
+other_y_value = [4,6,7,8]
+plt.plot(other_x_value,other_y_value, color="green")
+plt.title("sample plot")
+plt.xlabel("x value")
+plt.ylabel("y lable")
+
+
+plt.show()
